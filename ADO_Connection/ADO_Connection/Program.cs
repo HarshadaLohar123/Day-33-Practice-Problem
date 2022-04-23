f2e33cd1344ebfa226c1c1846ff61c14f8738497
harshadalohar123 <harshalohar123@gmail.com>
2022-04-23 17:06:35 +0530

    [Harshada]Added:ADO.Net Employee

---
+++ ADO_Connection/ADO_Connection/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ADO_Connection
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to ADO.net Program");
+            EmployeeRepo repo = new EmployeeRepo();
+            EmployeeModel employee = new EmployeeModel();
+            Employee.AddEmployee(employee);
+            repo.Employee();
+        }
+    }
+}
